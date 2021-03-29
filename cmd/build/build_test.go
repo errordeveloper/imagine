407b8aaa17714866ed1c5f4e2b6f89a93a3d2ae3
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-03-29 11:18:24 +0100

    Run an ephemeral registry container

--- cmd/build/build_test.go
+++ cmd/build/build_test.go
@@ -12,6 +12,11 @@ import (
 
 	. "github.com/errordeveloper/imagine/cmd/build"
 	"github.com/errordeveloper/imagine/pkg/buildx"
+
+	dockerTypes "github.com/docker/docker/api/types"
+	dockerContainer "github.com/docker/docker/api/types/container"
+	dockerClient "github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 )
 
 func TestBuildCmd(t *testing.T) {
@@ -19,7 +24,9 @@ func TestBuildCmd(t *testing.T) {
 
 	//g.Expect(os.Setenv(buildx.EnvImagineBuildxCommamnd, "./dummy-buildx.sh")).To(Succeed())
 
-	g.Expect(initBuilder()).To(Succeed())
+	h := &helper{}
+
+	g.Expect(h.initBuilder()).To(Succeed())
 
 	cmd := BuildCmd()
 
@@ -43,7 +50,7 @@ func TestBuildCmd(t *testing.T) {
 		{
 			args: []string{
 				"--config=" + testdata("sample-1.yaml"),
-				"--registry=host.docker.internal:5000/empty",
+				"--registry=" + h.registry() + "/empty",
 				"--debug",
 			},
 			fail: false,
@@ -52,7 +59,7 @@ func TestBuildCmd(t *testing.T) {
 		{
 			args: []string{
 				"--config=" + testdata("sample-1.yaml"),
-				"--registry=host.docker.internal:5000/test1",
+				"--registry=" + h.registry() + "/test1",
 				"--push",
 				"--debug",
 			},
@@ -62,7 +69,7 @@ func TestBuildCmd(t *testing.T) {
 		{
 			args: []string{
 				"--config=" + testdata("sample-1.yaml"),
-				"--registry=host.docker.internal:5000/test1",
+				"--registry=" + h.registry() + "/test1",
 				"--push",
 				"--debug",
 			},
@@ -80,27 +87,102 @@ func TestBuildCmd(t *testing.T) {
 		}
 		// TODO: check registry
 	}
+
+	h.cleanup()
 }
 
-func initBuilder() error {
+type helper struct {
+	stateDir, registryHost, registryPort, registryContainerID, buildkitdConfigPath string
+
+	docker *dockerClient.Client
+	buildx *buildx.Buildx
+}
+
+func (h *helper) initBuilder() error {
 	wd, _ := os.Getwd()
 	// go test runs in source dir, so we need to use
 	// top-level dir due to chdir in git.New
-	stateDir := filepath.Join(wd, "..", "..", ".imagine")
+	h.stateDir = filepath.Join(wd, "..", "..", ".imagine")
 
-	bx := buildx.New(stateDir)
-	bx.Debug = true
+	h.buildx = buildx.New(h.stateDir)
+	h.buildx.Debug = true
 
-	builderDescPath, err := bx.FindExisting()
+	builderDescPath, err := h.buildx.FindExisting()
 	if err != nil {
 		return err
 	}
 	if builderDescPath != "" {
-		_ = bx.Remove(builderDescPath)
-		_ = os.RemoveAll(stateDir)
+		_ = h.buildx.Remove(builderDescPath)
+		_ = os.RemoveAll(h.stateDir)
 	}
 
-	return bx.InitBuilder("", "--config="+testdata("buildkitd.toml"))
+	if err := h.startRegistry(); err != nil {
+		return err
+	}
+
+	h.buildkitdConfigPath = filepath.Join(h.stateDir, "buildkitd.toml")
+	buildkitdConfigContents := fmt.Sprintf("[registry.%q]\nhttp = true\ninsecure = true", h.registry())
+
+	if err := os.WriteFile(h.buildkitdConfigPath, []byte(buildkitdConfigContents), 0644); err != nil {
+		return err
+	}
+
+	return h.buildx.InitBuilder("", "--config="+h.buildkitdConfigPath)
+}
+
+func (h *helper) startRegistry() error {
+	ctx := context.Background()
+
+	docker, err := dockerClient.NewClientWithOpts(dockerClient.FromEnv, dockerClient.WithAPIVersionNegotiation())
+	if err != nil {
+		return err
+	}
+
+	config := &dockerContainer.Config{
+		Image:        "registry:2@sha256:9a2d4585a32de2df9aadc126708edd1da5f875093db6419d6894ddc2d1115d97",
+		ExposedPorts: nat.PortSet{"5000/tcp": struct{}{}},
+	}
+
+	if _, err := docker.ImagePull(ctx, config.Image, dockerTypes.ImagePullOptions{}); err != nil {
+		return err
+	}
+
+	resp, err := docker.ContainerCreate(ctx, config, &dockerContainer.HostConfig{PublishAllPorts: true}, nil, nil, "")
+	if err != nil {
+		return err
+	}
+
+	if err := docker.ContainerStart(ctx, resp.ID, dockerTypes.ContainerStartOptions{}); err != nil {
+		return err
+	}
+
+	h.registryContainerID = resp.ID
+
+	containerInfo, err := docker.ContainerInspect(ctx, resp.ID)
+	if err != nil {
+		return err
+	}
+
+	h.registryHost = "host.docker.internal" // TODO: this works on Docker for Desktop, check if it works in GitHub Actions
+	h.registryPort = containerInfo.NetworkSettings.Ports["5000/tcp"][0].HostPort
+
+	h.docker = docker
+
+	return nil
+}
+func (h *helper) registry() string {
+	return fmt.Sprintf("%s:%s", h.registryHost, h.registryPort)
+}
+
+func (h *helper) cleanup() {
+	_ = os.Remove(h.buildkitdConfigPath)
+
+	_ = h.docker.ContainerRemove(context.Background(), h.registryContainerID,
+		dockerTypes.ContainerRemoveOptions{RemoveVolumes: true, Force: true})
+
+	_ = h.buildx.Remove("")
+
+	_ = os.RemoveAll(h.stateDir)
 }
 
 func testdata(name string) string {
